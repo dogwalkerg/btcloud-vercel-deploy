d96277151d67319e8c08869008ef627c0fa07e52
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2026-06-25 19:35:54 +0800

    update domain to bt5.cnam.ccwu.cc

--- public/install/update_7.x_en.sh
+++ public/install/update_7.x_en.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+﻿#!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
-Btapi_Url='https://YOUR_DOMAIN.vercel.app'
+Btapi_Url='https://bt5.cnam.ccwu.cc'
 if [ ! -d /www/server/panel/BTPanel ];then
 	echo "============================================="
 	echo "Error, 5.x Can't use this command to upgrade!"
