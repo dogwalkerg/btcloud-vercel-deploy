fcc5390a495787c9cab3c8217a79ebcda0a56de1
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2026-06-25 19:35:52 +0800

    update domain to bt5.cnam.ccwu.cc

--- public/install/install_7.0_en.sh
+++ public/install/install_7.0_en.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+﻿#!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
-Btapi_Url='https://YOUR_DOMAIN.vercel.app'
+Btapi_Url='https://bt5.cnam.ccwu.cc'
 Check_Api=$(curl -Ss --connect-timeout 5 -m 2 $Btapi_Url/api/SetupCount)
 if [ "$Check_Api" != 'ok' ];then
 	Red_Error "The BT cloud could not be connected, so the installation process has been aborted!";
