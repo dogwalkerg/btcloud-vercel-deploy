a720b6d53038ab0f0a3b7f43d257f34301c20861
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2026-06-25 19:35:59 +0800

    update domain to bt5.cnam.ccwu.cc

--- public/install/update_btmonitor.sh
+++ public/install/update_btmonitor.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+﻿#!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
-Btapi_Url='https://YOUR_DOMAIN.vercel.app'
+Btapi_Url='https://bt5.cnam.ccwu.cc'
 
 monitor_path="/www/server/bt-monitor"
 run_bin="/www/server/bt-monitor/BT-MONITOR"
